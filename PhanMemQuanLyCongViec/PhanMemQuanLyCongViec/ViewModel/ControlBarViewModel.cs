ef111497fc07f98754b6058eb909d7e51f5adbc7
thinhvuthecreator <thinhvuh@gmail.com>
2023-03-14 17:29:28 +0700

    them 3 command thao tac voi cua so

--- PhanMemQuanLyCongViec/PhanMemQuanLyCongViec/ViewModel/ControlBarViewModel.cs
+++ PhanMemQuanLyCongViec/PhanMemQuanLyCongViec/ViewModel/ControlBarViewModel.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Controls;
+
+
 
 namespace PhanMemQuanLyCongViec.ViewModel
 {
@@ -11,11 +15,51 @@ namespace PhanMemQuanLyCongViec.ViewModel
     {
         #region commands
         public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
         #endregion
 
         public ControlBarViewModel()
         {
-            CloseWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => { });
+            CloseWindowCommand = new RelayCommand<object>((p) => { return p != null ? true : false; }, (p) => 
+            { Window window = ((Window)getParentWindow(p));
+                if(window != null)
+                {
+                    window.Close();
+                }
+            });
+            MaximizeWindowCommand = new RelayCommand<object>((p) => { return p != null ? true : false; }, (p) => {
+                Window window = ((Window)getParentWindow(p));
+                if (window != null)
+                {
+                    if (window.WindowState == WindowState.Maximized)
+                    { window.WindowState = WindowState.Normal; }
+                    else
+                    {
+                        window.WindowState = WindowState.Maximized;
+                    }
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand<object>((p) => { return p != null ? true : false; }, (p) => {
+                Window window = ((Window)getParentWindow(p));
+                if (window != null)
+                {
+                    window.WindowState = WindowState.Minimized;
+                }
+            });
         }
+
+        #region methods
+        FrameworkElement getParentWindow(object p)
+        {
+            FrameworkElement parent = p as FrameworkElement;
+            while(parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+        #endregion
+
     }
 }
--- PhanMemQuanLyCongViec/PhanMemQuanLyCongViec/ViewModel/MainViewModel.cs
+++ PhanMemQuanLyCongViec/PhanMemQuanLyCongViec/ViewModel/MainViewModel.cs
@@ -19,6 +19,7 @@ namespace PhanMemQuanLyCongViec.ViewModel
         public bool isActivated = false;
       public MainViewModel()
         {
+            
             LoginWindow loginWD = new LoginWindow();
             if (!isActivated)
             {
